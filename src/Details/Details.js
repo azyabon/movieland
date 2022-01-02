9477dd3e282a23b26b1b50eb7862d2f1940aafcc
azyabon <andrey.azyabin@bk.ru>
2022-01-02 22:37:44 +0700

    added videoplayer

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,32 +1,42 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import { observer } from 'mobx-react-lite';
 
 import styles from './Details.module.scss';
 import Actor from '../Actor/Actor';
 import actors from '../store/actors';
 import movies from "../store/movies";
+import YouTube from "react-youtube";
 
 const IMG_URL = `https://image.tmdb.org/t/p/original`;
 const NO_IMG = "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
+const API_KEY = "api_key=36d3b9492c7c489a5890ffdecffba2e5";
 
 const Details = observer((props) => {
+    const [trailer, setTrailer] = useState("");
 
     useEffect(() => {
         window.scrollTo(0, 0);
         actors.getActors();
         movies.getInfoAboutCurrentMovie();
+        getTrailer();
         }, [])
 
+    const getTrailer = async () => {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${localStorage.getItem("RMovieID")}/videos?${API_KEY}&language=en-US`)
+        const data = await response.json();
+        setTrailer(data.results[1].key);
+    }
+
     const putMovies = () => {
-        let LSmovies = props.local();
-        LSmovies = LSmovies.filter((elem) => {
+        let LS_movies = props.local();
+        LS_movies = LS_movies.filter((elem) => {
             if (elem.id === movies.currentMovie.id) {
                 return false
             }
             return true
         })
-        LSmovies.push({title: movies.currentMovie.original_title, poster: movies.currentMovie.poster_path, id: movies.currentMovie.id});
-        localStorage.setItem("movies", JSON.stringify(LSmovies));
+        LS_movies.push({title: movies.currentMovie.original_title, poster: movies.currentMovie.poster_path, id: movies.currentMovie.id});
+        localStorage.setItem("movies", JSON.stringify(LS_movies));
         alert(`${movies.currentMovie.original_title} has been added to your library`)
     }
 
@@ -57,6 +67,9 @@ const Details = observer((props) => {
                 <span><img className={styles.info__img} src="./money-bag.png" alt="moneybag" />Budget: ${movies.currentMovie.budget === 0 ? "unknown" : movies.currentMovie.budget}</span>
                 <span><img className={styles.info__img} src="./tickets.png" alt="tickets" />Revenue: ${movies.currentMovie.revenue === 0 ? "unknown" : movies.currentMovie.revenue}</span>
             </div>
+            <section className={styles.videoplayer}>
+                <YouTube videoId={trailer} />
+            </section>
             <section className={styles.actors}>
                 {actors.actors.map(actor => {
                     return <Actor key={actor.id} {...actor} />
